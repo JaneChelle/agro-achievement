d70dc03446ce62b89688325a50b433f44eb69c97
wangjingxiao <2962784374@qq.com>
2019-02-14 15:09:25 +0800

    gdfg

---
+++ src/main/resources/static/js/center.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    //菜单切换
+    $("#register").click(function () {
+        $(".ifm").attr("src","register.html");
+    });
+    $("#result1").click(function () {
+        $(".ifm").attr("src","result1.html");
+    });
+    $("#result2").click(function () {
+        $(".ifm").attr("src","result2.html");
+    });
+    $("#supply").click(function () {
+        $(".ifm").attr("src","supply.html");
+    });
+    $("#supplyg").click(function () {
+        $(".ifm").attr("src","supplyg.html");
+    });
+    $("#expertInfo").click(function () {
+        $(".ifm").attr("src","expert.html");
+    });
+//设置高亮
+    $(".addColor").on('click',function () {
+        $(".addColor").css("backgroundColor","#282B33");
+        $(this).css("backgroundColor","#009688");
+    })
+    //设置滑动
+    $(".list_one").click(function(){
+        $(this).siblings(".list_two").slideToggle("1s");
+        $(this).parent().siblings().children(".list_two").slideUp("1s");
+
+    });
+
+});

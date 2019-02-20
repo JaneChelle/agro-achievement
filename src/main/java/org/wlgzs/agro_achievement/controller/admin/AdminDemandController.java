a8b768e4d0495e34d00ff6dfa4ebf0f2dba194fe
wangjingxiao <2962784374@qq.com>
2019-02-20 17:45:16 +0800

    鬼地方个

--- src/main/java/org/wlgzs/agro_achievement/controller/admin/AdminDemandController.java
+++ src/main/java/org/wlgzs/agro_achievement/controller/admin/AdminDemandController.java
@@ -1,28 +1,28 @@
-package org.wlgzs.agro_achievement.controller.admin;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.wlgzs.agro_achievement.base.BaseController;
-
-/**
- * @author:胡亚星
- * @createTime 2019-02-20 17:07
- * @description:
- **/
-@RestController
-@RequestMapping("/AdminDemand")
-public class AdminDemandController extends BaseController {
-
-    //查看所有需求(搜索)
-    @RequestMapping(value = "/adminAdminDemand")
-    public ModelAndView adminAdminDemand(Model model, @RequestParam(value = "current", defaultValue = "1") int current,
-                                         @RequestParam(value = "limit", defaultValue = "8") int limit,
-                                         @RequestParam(name = "findName",defaultValue = "") String findName){
-
-    }
-
-}
+//package org.wlgzs.agro_achievement.controller.admin;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.wlgzs.agro_achievement.base.BaseController;
+//
+///**
+// * @author:胡亚星
+// * @createTime 2019-02-20 17:07
+// * @description:
+// **/
+//@RestController
+//@RequestMapping("/AdminDemand")
+//public class AdminDemandController extends BaseController {
+//
+//    //查看所有需求(搜索)
+//    @RequestMapping(value = "/adminAdminDemand")
+//    public ModelAndView adminAdminDemand(Model model, @RequestParam(value = "current", defaultValue = "1") int current,
+//                                         @RequestParam(value = "limit", defaultValue = "8") int limit,
+//                                         @RequestParam(name = "findName",defaultValue = "") String findName){
+//
+//    }
+//
+//}
--- src/main/java/org/wlgzs/agro_achievement/service/impl/DemandServiceImpl.java
+++ src/main/java/org/wlgzs/agro_achievement/service/impl/DemandServiceImpl.java
@@ -1,115 +1,115 @@
-package org.wlgzs.agro_achievement.service.impl;
-
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import org.wlgzs.agro_achievement.entity.Demand;
-import org.wlgzs.agro_achievement.mapper.DemandMapper;
-import org.wlgzs.agro_achievement.service.IDemandService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
-import org.wlgzs.agro_achievement.util.Result;
-import org.wlgzs.agro_achievement.util.ResultCode;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.List;
-
-/**
- * <p>
- * 服务实现类
- * </p>
- *
- * @author 胡亚星
- * @since 2019-01-19
- */
-@Service
-public class DemandServiceImpl extends ServiceImpl<DemandMapper, Demand> implements IDemandService {
-
-    //发布需求
-    @Override
-    public Result addDemand(Demand demand) {
-        if (demand != null) {
-            //获取现在时间
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            LocalDateTime time = LocalDateTime.now();
-            String localTime = formatter.format(time);
-            LocalDateTime ldt = LocalDateTime.parse(localTime, formatter);
-            demand.setStatusCode("0");//需要审核
-            demand.setReleaseTime(ldt);
-            demand.setPageView(0);
-            baseMapper.insert(demand);
-            return new Result(ResultCode.SUCCESS, "发布成功！");
-        }
-        return new Result(ResultCode.FAIL, "请输入正确的信息！");
-    }
-
-    //删除需求
-    @Override
-    public Result deleteDemand(Integer demandId) {
-        Demand demand = baseMapper.selectById(demandId);
-        if (demand != null) {
-            baseMapper.deleteById(demandId);
-            return new Result(ResultCode.SUCCESS, "删除成功！");
-        }
-        return new Result(ResultCode.FAIL, "不存在！");
-    }
-
-    //修改需求
-    @Override
-    public Result modifyDemand(Demand demand) {
-        if (demand != null) {
-            Demand demand1 = baseMapper.selectById(demand.getDemandId());
-            if (demand1 != null) {
-                demand.setReleaseTime(demand1.getReleaseTime());
-                demand.setStatusCode(demand1.getStatusCode());
-                demand.setPageView(demand1.getPageView());
-                baseMapper.updateById(demand);
-                return new Result(ResultCode.SUCCESS,"修改成功！");
-            }
-            return new Result(ResultCode.FAIL,"该条记录不存在！");
-        }
-        return new Result(ResultCode.FAIL,"操作失败！");
-    }
-
-    //按照用户查询所有需求（状态码）
-    @Override
-    public Result selectDemand(Integer userId, String statusCode,int current,int limit) {
-        List<Demand> demandList = null;
-        QueryWrapper<Demand> queryWrapper = new QueryWrapper();
-        IPage<Demand> iPage = null;
-        if(statusCode == null || statusCode.equals("")){  //查询所有
-            queryWrapper.eq("user_id",userId);
-            Page page = new Page(current, limit);
-            iPage = baseMapper.selectPage(page, queryWrapper);
-            demandList = iPage.getRecords();
-            return new Result(ResultCode.SUCCESS, "",demandList, iPage.getPages(), iPage.getCurrent());
-        }else{
-            queryWrapper.and(i -> i.eq("user_id",userId).eq("status_code",statusCode));
-            Page page = new Page(current, limit);
-            iPage = baseMapper.selectPage(page, queryWrapper);
-            demandList = iPage.getRecords();
-            return new Result(ResultCode.SUCCESS, "",demandList, iPage.getPages(), iPage.getCurrent());
-        }
-    }
-
-    //查看需求详情
-    @Override
-    public Result demandDetails(Integer demandId) {
-        Demand demand = baseMapper.selectById(demandId);
-        if(demand != null){
-            return new Result(ResultCode.SUCCESS,demand);
-        }
-        return new Result(ResultCode.FAIL,"不存在！");
-    }
-
-    //搜索所有需求
-    @Override
-    public Result adminDemandList(String findName, int current, int limit) {
-
-    }
-
-
-}
+//package org.wlgzs.agro_achievement.service.impl;
+//
+//import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+//import com.baomidou.mybatisplus.core.metadata.IPage;
+//import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+//import org.wlgzs.agro_achievement.entity.Demand;
+//import org.wlgzs.agro_achievement.mapper.DemandMapper;
+//import org.wlgzs.agro_achievement.service.IDemandService;
+//import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+//import org.springframework.stereotype.Service;
+//import org.wlgzs.agro_achievement.util.Result;
+//import org.wlgzs.agro_achievement.util.ResultCode;
+//
+//import java.text.SimpleDateFormat;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//import java.util.Date;
+//import java.util.List;
+//
+///**
+// * <p>
+// * 服务实现类
+// * </p>
+// *
+// * @author 胡亚星
+// * @since 2019-01-19
+// */
+//@Service
+//public class DemandServiceImpl extends ServiceImpl<DemandMapper, Demand> implements IDemandService {
+//
+//    //发布需求
+//    @Override
+//    public Result addDemand(Demand demand) {
+//        if (demand != null) {
+//            //获取现在时间
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//            LocalDateTime time = LocalDateTime.now();
+//            String localTime = formatter.format(time);
+//            LocalDateTime ldt = LocalDateTime.parse(localTime, formatter);
+//            demand.setStatusCode("0");//需要审核
+//            demand.setReleaseTime(ldt);
+//            demand.setPageView(0);
+//            baseMapper.insert(demand);
+//            return new Result(ResultCode.SUCCESS, "发布成功！");
+//        }
+//        return new Result(ResultCode.FAIL, "请输入正确的信息！");
+//    }
+//
+//    //删除需求
+//    @Override
+//    public Result deleteDemand(Integer demandId) {
+//        Demand demand = baseMapper.selectById(demandId);
+//        if (demand != null) {
+//            baseMapper.deleteById(demandId);
+//            return new Result(ResultCode.SUCCESS, "删除成功！");
+//        }
+//        return new Result(ResultCode.FAIL, "不存在！");
+//    }
+//
+//    //修改需求
+//    @Override
+//    public Result modifyDemand(Demand demand) {
+//        if (demand != null) {
+//            Demand demand1 = baseMapper.selectById(demand.getDemandId());
+//            if (demand1 != null) {
+//                demand.setReleaseTime(demand1.getReleaseTime());
+//                demand.setStatusCode(demand1.getStatusCode());
+//                demand.setPageView(demand1.getPageView());
+//                baseMapper.updateById(demand);
+//                return new Result(ResultCode.SUCCESS,"修改成功！");
+//            }
+//            return new Result(ResultCode.FAIL,"该条记录不存在！");
+//        }
+//        return new Result(ResultCode.FAIL,"操作失败！");
+//    }
+//
+//    //按照用户查询所有需求（状态码）
+//    @Override
+//    public Result selectDemand(Integer userId, String statusCode,int current,int limit) {
+//        List<Demand> demandList = null;
+//        QueryWrapper<Demand> queryWrapper = new QueryWrapper();
+//        IPage<Demand> iPage = null;
+//        if(statusCode == null || statusCode.equals("")){  //查询所有
+//            queryWrapper.eq("user_id",userId);
+//            Page page = new Page(current, limit);
+//            iPage = baseMapper.selectPage(page, queryWrapper);
+//            demandList = iPage.getRecords();
+//            return new Result(ResultCode.SUCCESS, "",demandList, iPage.getPages(), iPage.getCurrent());
+//        }else{
+//            queryWrapper.and(i -> i.eq("user_id",userId).eq("status_code",statusCode));
+//            Page page = new Page(current, limit);
+//            iPage = baseMapper.selectPage(page, queryWrapper);
+//            demandList = iPage.getRecords();
+//            return new Result(ResultCode.SUCCESS, "",demandList, iPage.getPages(), iPage.getCurrent());
+//        }
+//    }
+//
+//    //查看需求详情
+//    @Override
+//    public Result demandDetails(Integer demandId) {
+//        Demand demand = baseMapper.selectById(demandId);
+//        if(demand != null){
+//            return new Result(ResultCode.SUCCESS,demand);
+//        }
+//        return new Result(ResultCode.FAIL,"不存在！");
+//    }
+//
+//    //搜索所有需求
+//    @Override
+//    public Result adminDemandList(String findName, int current, int limit) {
+//
+//    }
+//
+//
+//}

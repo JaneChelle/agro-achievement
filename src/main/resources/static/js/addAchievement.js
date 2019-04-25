b45fe2cab43a9ec20c25140b11b04af7fdcd06d7
2962784374@qq.com <2962784374@qq.com>
2019-04-25 21:58:25 +0800

    pull

--- src/main/resources/static/js/addAchievement.js
+++ src/main/resources/static/js/addAchievement.js
@@ -5,7 +5,7 @@ function fomrReset()
 //手机号的验证
 function phone()
 {
-    var phone = document.getElementById('phone').value;
+    var phone = document.getElementsByClassName('phone').value;
     if(!(/^1[34578]\d{9}$/.test(phone))){
         alert("手机号码有误，请重填");
         return false;
@@ -57,3 +57,177 @@ function fomrReset()
 {
     document.getElementById("myform").reset();
 }
+//添加成果
+function submitAchievement() {
+    var achievementName=$('.achievementName').val();
+    var achievementIntroduce=$('.achievementIntroduce').val();
+    var typeName=$('.typeName').val();
+    var achievementKey=$('.achievementKey').val();
+    var start_time=$('.start_time').val();
+    var end_time=$('.end_time').val();
+    var propertyAddress=$('.propertyAddress').val();
+    var awards=$('.awards').val();
+    var expectedPrice=$('.expectedPrice').val();
+    var linkman=$('.linkman').val();
+    var phone=$('.phone').val();
+    var propertyIntroduce=$('.propertyIntroduce').val();
+    var email=$('.email').val();
+    var contactAddress=$('.contactAddress').val();
+    var propertyNumber=$('.propertyNumber').val();
+    var cellNumber=$('.cellNumber').val();
+    var file=$('.pic img').src;
+    if(achievementName ==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('成果名称不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(achievementIntroduce==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('成果介绍不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(typeName==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('成果类型不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');$('.cure').html(' ');
+        },2000);
+    }else if(achievementKey==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('成果关键字不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');$('.cure').html(' ');
+        },2000);
+    }else if(start_time==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('开始时间不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');$('.cure').html(' ');
+        },2000);
+    }else if(end_time==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('结束时间不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');$('.cure').html(' ');
+        },2000);
+    }else if(propertyAddress==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('产权归属地不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');$('.cure').html(' ');
+        },2000);
+    }else if(awards==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('获奖情况不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');$('.cure').html(' ');
+        },2000);
+    }else if(expectedPrice==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('预期交易价格不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(linkman==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('联系人不能为空');
+        setTimeout(function () {
+            $('.cure').html(' ');
+            $('.cure').removeClass('uu');
+        },2000);
+    }else if(phone==''){
+        $('.cure').addClass('uu');
+        $('.cure').html('联系电话不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(propertyIntroduce == ''){
+        $('.cure').addClass('uu');
+        $('.cure').html('知识产权说明不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(email == ''){
+        $('.cure').addClass('uu');
+        $('.cure').html('邮箱不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(contactAddress == ''){
+        $('.cure').addClass('uu');
+        $('.cure').html('联系地址不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(propertyNumber == ''){
+        $('.cure').addClass('uu');
+        $('.cure').html('知识产权编号不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(cellNumber == ''){
+        $('.cure').addClass('uu');
+        $('.cure').html('手机号不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }else if(file == ''){
+        $('.cure').addClass('uu');
+        $('.cure').html('图片不能为空');
+        setTimeout(function () {
+            $('.cure').removeClass('uu');
+            $('.cure').html(' ');
+        },2000);
+    }
+    else{
+        $.ajax({
+            type: "post",
+            url: "/achievement/addAchievement",
+            data: $('#myform').serialize(),
+            async: false,
+            success: function (data) {
+                // $('.cure').addClass('uu');
+                // $('.cure').html(data.msg);
+                // setTimeout(function () {
+                //     $('.cure').removeClass('uu');
+                // },2000);
+                // setTimeout(function () {
+                //     location.reload(true);
+                // },1000);
+                if (data.code = 0){
+                    window.location.href = '/achievement/selectAchievement?statusCode=0';
+                    alert('成功')
+                }else{
+                    window.location.href = '/achievement/toAddAchievement';
+                    alert('发布失败')
+                }
+
+            },
+            error: function (data) {
+                $('.cure').addClass('uu');
+                $('.cure').html(data.msg);
+                setTimeout(function () {
+                    $('.cure').removeClass('uu');
+                },1000);
+                // setTimeout(function () {
+                //     location.reload(true);
+                // },500);
+
+                alert('异常')
+            }
+        });
+        return true;
+    }
+
+};

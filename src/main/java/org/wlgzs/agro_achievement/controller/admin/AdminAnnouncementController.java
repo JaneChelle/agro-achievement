8f109deb86fa29c934d6f7a781d63bca5c51872e
BYwangbing <1787583716@qq.com>
2019-04-27 21:21:46 +0800

    Prompt

--- src/main/java/org/wlgzs/agro_achievement/controller/admin/AdminAnnouncementController.java
+++ src/main/java/org/wlgzs/agro_achievement/controller/admin/AdminAnnouncementController.java
@@ -2,6 +2,8 @@ package org.wlgzs.agro_achievement.controller.admin;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,11 +42,11 @@ public class AdminAnnouncementController extends BaseController {
 
     //查看公告详情（管理员）
     @RequestMapping("/announcementAdminDetails")
-    public ModelAndView announcementAdminDetails(Model model,Integer announcementId){
+    public ModelAndView announcementAdminDetails(Model model, Integer announcementId) {
         Announcement announcement = iAnnouncementService.announcementDetails(announcementId);
-        model.addAttribute("announcement",announcement);
-        if(announcement == null){
-            model.addAttribute("msg","数据不存在！");
+        model.addAttribute("announcement", announcement);
+        if (announcement == null) {
+            model.addAttribute("msg", "数据不存在！");
             return new ModelAndView("redirect:/admin/selectAnnouncement");
         }
         return new ModelAndView("admin/Announcement");
@@ -52,11 +54,11 @@ public class AdminAnnouncementController extends BaseController {
 
     //修改公告详情（管理员）
     @RequestMapping("/announcementAdminModify")
-    public ModelAndView announcementAdminModify(Model model,Integer announcementId){
+    public ModelAndView announcementAdminModify(Model model, Integer announcementId) {
         Announcement announcement = iAnnouncementService.announcementDetails(announcementId);
-        model.addAttribute("announcement",announcement);
-        if(announcement == null){
-            model.addAttribute("msg","数据不存在！");
+        model.addAttribute("announcement", announcement);
+        if (announcement == null) {
+            model.addAttribute("msg", "数据不存在！");
             return new ModelAndView("redirect:/admin/selectAnnouncement");
         }
         return new ModelAndView("admin/modifyAnnouncement");
@@ -71,50 +73,54 @@ public class AdminAnnouncementController extends BaseController {
 
     //删除公告
     @RequestMapping(value = "/deleteAnnouncement")
-    public Result deleteAnnouncement(Integer announcementId,Model model) {
+    public Result deleteAnnouncement(Integer announcementId, Model model) {
         Result result = iAnnouncementService.deleteAnnouncement(announcementId);
         return result;
     }
 
     //按类别查询公告(默认查询所有的)
     @GetMapping("/selectAnnouncement")
-    public ModelAndView selectAnnouncement(Model model,@RequestParam(value = "findName",defaultValue = "")String findName,
-            @RequestParam(value = "announcementType", defaultValue = "") String announcementType,
-            @RequestParam(value = "current", defaultValue = "1") Integer current,
-            @RequestParam(value = "limit", defaultValue = "8") Integer limit) {
+    public ModelAndView selectAnnouncement(Model model, @RequestParam(value = "findName", defaultValue = "") String findName,
+                                           @RequestParam(value = "announcementType", defaultValue = "") String announcementType,
+                                           @RequestParam(value = "current", defaultValue = "1") Integer current,
+                                           @RequestParam(value = "limit", defaultValue = "8") Integer limit) {
         Result result = iAnnouncementService.selectAnnouncement(findName, current, limit);
         List<Announcement> announcementList = (List<Announcement>) result.getData();
-        model.addAttribute("announcementList",announcementList);
+        model.addAttribute("announcementList", announcementList);
         model.addAttribute("TotalPages", result.getPages());//总页数
         model.addAttribute("Number", result.getCurrent());//当前页数
-        model.addAttribute("findName",findName);
+        model.addAttribute("findName", findName);
 
         return new ModelAndView("admin/adminAnnouncement");
     }
 
     //按公告是否显示查询
     @RequestMapping("/selectByIsShow")
-    public ModelAndView selectByIsShow(@RequestParam(value = "isShow" ,defaultValue = "3") int isShow,Model model,@RequestParam(value = "current", defaultValue = "1") Integer current,
-                                       @RequestParam(value = "limit", defaultValue = "8") Integer limit){
+    public ModelAndView selectByIsShow(@RequestParam(value = "isShow", defaultValue = "3") int isShow, Model model, @RequestParam(value = "current", defaultValue = "1") Integer current,
+                                       @RequestParam(value = "limit", defaultValue = "8") Integer limit) {
+        Page page = new Page(current, limit);
         QueryWrapper<Announcement> queryWrapper = new QueryWrapper<>();
-        if(isShow == 0 || isShow == 1){
-            queryWrapper.eq("is_show",isShow);
+        if (isShow == 0 || isShow == 1) {
+            queryWrapper.eq("is_show", isShow);
         }
-        List<Announcement> announcementList = iAnnouncementService.list(queryWrapper);
-        model.addAttribute("announcementList",announcementList);
+        IPage<Announcement> iPage = iAnnouncementService.page(page, queryWrapper);
+        List<Announcement> announcementList = iPage.getRecords();
+        model.addAttribute("announcementList", announcementList);
+        model.addAttribute("TotalPages", iPage.getPages());//总页数
+        model.addAttribute("Number", iPage.getCurrent());//当前页数
         return new ModelAndView("admin/adminAnnouncement");
     }
 
     //修改公告是否显示
     @RequestMapping("/modifiedAccording")
-    public Result modifiedAccording(int announcementId,int isShow){
+    public Result modifiedAccording(int announcementId, int isShow) {
         Announcement announcement = iAnnouncementService.getById(announcementId);
-        if(announcement != null){
+        if (announcement != null) {
             announcement.setIsShow(isShow);
             iAnnouncementService.updateById(announcement);
-            return new Result(ResultCode.SUCCESS,"修改成功！");
+            return new Result(ResultCode.SUCCESS, "修改成功！");
         }
-        return new Result(ResultCode.FAIL,"不存在！");
+        return new Result(ResultCode.FAIL, "不存在！");
     }
 
 }
